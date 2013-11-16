ef67f37d97ad159945dd4fee8b26672fb1584184
djey <j.canler@gmail.com>
2013-11-16 21:51:40 +0100

    [SMRT-3.1] Renamed size attribute to include unit

--- web-services/rupees/model/disk_details.rb
+++ web-services/rupees/model/disk_details.rb
@@ -6,15 +6,15 @@ class DiskDetails
 
   attr_reader :id
   attr_reader :model
-  attr_reader :size
+  attr_reader :size_gigabytes
   attr_reader :temperature_celsius
   attr_reader :smart_status
   attr_reader :smartx_status
 
-  def initialize(id = -1, model = 'N/A', size = -1, temperature_celsius = -1, smart_status = 'N/A', smartx_status = 'N/A')
+  def initialize(id = -1, model = 'N/A', size_gigabytes = -1, temperature_celsius = -1, smart_status = 'N/A', smartx_status = 'N/A')
     @id = id
     @model = model
-    @size = size
+    @size_gigabytes = size_gigabytes
     @temperature_celsius = temperature_celsius
     @smart_status = smart_status
     @smartx_status = smartx_status
@@ -24,7 +24,7 @@ class DiskDetails
     {
         :id => @id,
         :model => @model,
-        :size => @size,
+        :size_gigabytes => @size_gigabytes,
         :temperature_celsius => @temperature_celsius,
         :smart_status => @smart_status,
         :smartx_status => @smartx_status
