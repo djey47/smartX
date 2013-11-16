22a6e5869ddee54e2577d79729222c8eeb8670f9
djey <j.canler@gmail.com>
2013-11-16 17:27:41 +0100

    [SMRT-4] static design (wip)

---
+++ web-ui/javascripts/smartx.js
@@ -0,0 +1,8 @@
+//window.onload = setupRefresh;
+//
+//function setupRefresh() {
+//    setTimeout("refreshPage();", 5000);
+//}
+//function refreshPage() {
+//    window.location = location.href;
+//}
