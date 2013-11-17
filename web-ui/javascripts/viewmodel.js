1b2410adde83978269706cd8a43e87f1755f23b8
djey <j.canler@gmail.com>
2013-11-17 23:03:34 +0100

    [SMRT-4.3] Getting disk list from web service

--- web-ui/javascripts/viewmodel.js
+++ web-ui/javascripts/viewmodel.js
@@ -2,13 +2,22 @@ var diskListViewModel = {
 
     // Attributes
     lastReceived: ko.observable(''),
-    disks: new Array(),
+
+    disks: ko.observableArray(new Array()),
 
     // Methods
     fetch: function() {
         $.getJSON("http://localhost:4600/disks.json", function(data) {
             diskListViewModel.lastReceived(data.last_received);
+
+            $.each(data.disks, function(index, disk){
+                diskListViewModel.disks.push(disk);
+            });
         })
+    },
+
+    showSmartDetails: function() {
+        $("#smartPopup").modal("show");
     }
 };
 
