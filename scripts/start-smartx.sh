8cc131239f3b6b4bbe7f1da3d18cbb1675ee6408
djey <j.canler@gmail.com>
2014-02-12 09:16:43 +0100

    Moved startup scripts to devconf repo

--- scripts/start-smartx.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-# Run script for deployment environment
-echo Starting smartX web services...
-nohup ruby ./smartx-services/rupees/smart_x.rb > smartx-services.log &
-echo $! > smartx-services.pid
-echo Logs: tail -f smartx-services.log
