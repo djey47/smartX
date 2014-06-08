273b4c154b05a2b67bd04330eace12e9f4f5b8b8
djey <j.canler@gmail.com>
2014-06-09 01:47:56 +0200

    invokeAndRepeat function now returns intervalId

--- web-ui/javascripts/smartx.js
+++ web-ui/javascripts/smartx.js
@@ -1,9 +1,17 @@
 // Utilities
+
+/*
+* Invokes specified function immediately, then every intervalMilliseconds.
+* Returns intervalId to control it later on
+*/
 function invokeAndRepeat(toInvoke, intervalMilliseconds) {
     toInvoke();
-    setInterval(toInvoke, intervalMilliseconds);
+    return setInterval(toInvoke, intervalMilliseconds);
 }
 
+/*
+* Returns fahrenheit degrees temperature from celsius degrees.
+*/
 function celsiusToFahrenheit(celsiusTemp) {
     return (celsiusTemp * 1.8 + 32).toFixed(1);
 }
